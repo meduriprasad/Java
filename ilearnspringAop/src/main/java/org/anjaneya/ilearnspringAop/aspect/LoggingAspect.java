81477360c1f70dfdca821d734a0def98bfe598b6
Anjaneya Meduri <ilearn.pvbgroup@gmail.com>
2018-02-24 12:45:42 +0530

    changes

--- ilearnspringAop/src/main/java/org/anjaneya/ilearnspringAop/aspect/LoggingAspect.java
+++ ilearnspringAop/src/main/java/org/anjaneya/ilearnspringAop/aspect/LoggingAspect.java
@@ -2,6 +2,7 @@ package org.anjaneya.ilearnspringAop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
@@ -9,5 +10,15 @@ public class LoggingAspect {
 	@Before("execution(public String getName())")
 	public void LoggingAdvice(){
 		System.out.println("Advice run..get method called");
+	}
+	
+	@Before("allGetters()")
+	public void LoggingAdvice1(){
+		System.out.println("Advice run..get method with zero or more arguments");
+	}
+	
+	@Pointcut("execution(* get*())")
+	public void allGetters(){
+		System.out.println("Advice run..point cut...");
 	}
 }
